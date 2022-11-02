9d757caa4290aaadbe8425e30b208c8b47e0f568
Maria Hagelin <mthagelin@gmail.com>
2022-11-02 20:42:51 +0100

    initial commit

---
+++ src/JButtonArrayClass.java
@@ -0,0 +1,109 @@
+import javax.swing.*;
+
+public class JButtonArrayClass extends JFrame {
+    int rows = 0;
+    int columns = 0;
+    String[] arrayNumbersString = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", " "};
+    JButtonArrayClass(int rows, int columns){
+        JButton[][] myMultiArray = new JButton[rows][columns];
+        int räknare = 0;
+        for (int row = 0; row < myMultiArray.length; row++) {
+            for (int column = 0; column < myMultiArray[row].length; column++) {
+                myMultiArray[row][column]=new JButton(arrayNumbersString[räknare]);
+          //      System.out.println(myMultiArray[row][column].getText());
+                räknare++;
+            }
+        }
+    }
+    public JButton[][] returnJButtonArray(String buttonNumber, JButton[][] buttons){
+        int rowOfButton = 0;
+        int columnOfButton = 0;
+        for (int row = 0; row < buttons.length; row++) {//mindre än 4...
+            for (int column = 0; column < buttons[row].length; column++) {//mindre än 4...
+                if(buttons[row][column].getText().equals(buttonNumber)){
+                    rowOfButton=row;
+                    columnOfButton=column;
+                  //  System.out.println("row of button: " + rowOfButton + " column of button: " + columnOfButton);
+                    buttons = swapPlacesInArray(rowOfButton, columnOfButton, buttons);
+                    return buttons;
+                }
+            }
+        }
+        return buttons;
+    }
+
+    public JButton[][] swapPlacesInArray(int rowOfArray, int columnOfArray, JButton[][] myMultiArray) {
+        JButtonSwapPlacesInArray a = new JButtonSwapPlacesInArray();
+        JButton[][] placementOfZero = new JButton[4][4];
+        int rowi = 0;
+        int columni = 0;
+        for (int row = 0; row < myMultiArray.length; row++) {
+            for (int column = 0; column < myMultiArray[row].length; column++) {
+                if (myMultiArray[row][column].getText().equals(" ")) {
+                    placementOfZero[row][column] = myMultiArray[row][column];
+                  //  System.out.println(placementOfZero[row][column].getText() + " is of position row: " + row + " column: " + column);
+                    rowi = row;
+                    columni = column;
+                }
+            }
+        }
+
+        JButton[][] tempArrayFromPlacementOfZero = new JButton[rowi][columni];
+        JButton[][] tempArrayFromInput = new JButton[rowOfArray][columnOfArray];
+
+        int tempRow = rowi;
+        int tempColumn = columni;
+
+    //    System.out.println("Placement of zero, row: " + rowi + " column: " + columni);
+     //   System.out.println("Placement of input, row: " + rowOfArray + " column: " + columnOfArray);
+        if (rowOfArray == rowi + 1 && columnOfArray == columni) {
+            rowi = rowOfArray; //inputs föredetta plats
+            rowOfArray = tempRow;//nollans föredetta plats
+            columni = columnOfArray; //inputs föredetta plats
+            columnOfArray = tempColumn;//nollans föredetta plats
+         //   System.out.println("New placement of zero, row: " + rowi + " column: " + columni + " \nNew placement of input,  row: " + rowOfArray + " column: " + columnOfArray);
+            return a.theJButtonArraySwap(myMultiArray[rowi][columni], myMultiArray[rowOfArray][columnOfArray], rowOfArray, columnOfArray, rowi, columni, myMultiArray);
+        } else if (rowOfArray == rowi - 1 && columnOfArray == columni) {//raden ovanför
+            rowi = rowOfArray;
+            rowOfArray = tempRow;
+            columni = columnOfArray;
+            columnOfArray = tempColumn;
+          //  System.out.println("New placement of zero, row: " + rowi + " column: " + columni + " \nNew placement of input,  row: " + rowOfArray + " column: " + columnOfArray);
+            return a.theJButtonArraySwap(myMultiArray[rowi][columni], myMultiArray[rowOfArray][columnOfArray], rowOfArray, columnOfArray, rowi, columni, myMultiArray);
+        } else if (rowOfArray == rowi && columnOfArray == columni + 1) {//kolumnen till höger
+            rowi = rowOfArray;
+            rowOfArray = tempRow;
+            columni = columnOfArray;
+            columnOfArray = tempColumn;
+            //System.out.println("New placement of zero, row: " + rowi + " column: " + columni + " \nNew placement of input,  row: " + rowOfArray + " column: " + columnOfArray);
+            return a.theJButtonArraySwap(myMultiArray[rowi][columni], myMultiArray[rowOfArray][columnOfArray], rowOfArray, columnOfArray, rowi, columni, myMultiArray);
+        } else if (rowOfArray == rowi && columnOfArray == columni - 1) {//kolumnen till vänster
+            rowi = rowOfArray;
+            rowOfArray = tempRow;
+            columni = columnOfArray;
+            columnOfArray = tempColumn;
+          //  System.out.println("New placement of zero, row: " + rowi + " column: " + columni + " \nNew placement of input,  row: " + rowOfArray + " column: " + columnOfArray);
+            return a.theJButtonArraySwap(myMultiArray[rowi][columni], myMultiArray[rowOfArray][columnOfArray], rowOfArray, columnOfArray, rowi, columni, myMultiArray);
+        }
+        return myMultiArray;
+    }
+
+    public static void main(String[] args) {
+       /* boolean run=true;
+        while (run==true) {
+            try {
+                int rows = Integer.parseInt(JOptionPane.showInputDialog("Rows? Between 4 and 7"));
+                int columns = Integer.parseInt(JOptionPane.showInputDialog("Columns? Between 4 and 7"));
+                if((4<=rows && rows<=7 && 4<=columns && columns<=7)) {
+                    PlayGUI fifteenGame = new PlayGUI(rows, columns);
+                    run = false;
+                }else {
+                    JOptionPane.showMessageDialog(null, "Sorry, you messed up. We'll try again!");
+                }
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, "So... input is messed up.");
+                }
+                }*/ //If one wishes to add some custom rows and columns, it may be added but needs additional steps. I would make a new constructor for PlayGUI etc.
+
+    }
+}
---
+++ src/PlayGUI.java
@@ -0,0 +1,220 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.*;
+import java.util.List;
+
+import static javax.swing.SwingConstants.CENTER;
+
+public class PlayGUI extends JFrame implements ActionListener, MouseListener{
+    Border border = BorderFactory.createLineBorder(new Color(65, 136, 109, 255), 1);
+    int r=65;
+    int g=136;
+    int b=123;
+    int rows = 4;
+    int columns = 4;
+    List<String> stringList = Arrays.asList("1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", " ");
+    String[] arrayNumbersString = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", " "};
+
+    JButton[][] buttons = new JButton[rows][columns];
+    JPanel spelGrid = new JPanel();
+    JButton newGameButton = new JButton("\uD83C\uDF43 N E W  G A M E \uD83C\uDF43");
+    JPanel panel = new JPanel();
+
+    JLabel win = new JLabel("");
+
+    public PlayGUI() {
+        setTitle("★ \uD835\uDFED\uD835\uDFF1 ★");
+        spelGrid.setLayout(new GridLayout(rows, columns));
+        int räknare=0;
+
+        for (int row = 0; row < buttons.length; row++) {
+            for (int column = 0; column < buttons[row].length; column++) {
+                buttons[row][column]=new JButton(arrayNumbersString[räknare]);
+                buttons[row][column].setBackground(new Color(0xBAC2BA));
+                spelGrid.add(buttons[row][column]);
+                buttons[row][column].addActionListener(this);
+                räknare++;
+                buttons[row][column].setForeground(new Color(7, 26, 14, 163));
+                buttons[row][column].setFont(new Font("Bold Serif", Font.BOLD, 16));
+                buttons[row][column].setBorder(border);
+                buttons[row][column].setFocusPainted(false);
+                buttons[row][column].addMouseListener(this);//not using mouselistener for buttons...
+            }
+        }
+
+        panel.setLayout(new BorderLayout());
+        panel.add(newGameButton, BorderLayout.NORTH);
+        panel.add(spelGrid, BorderLayout.CENTER);
+
+        panel.add(win, BorderLayout.SOUTH);
+        newGameButton.addActionListener(this);
+        newGameButton.setBackground((new Color(0x162D24)));
+        newGameButton.setForeground(new Color(186, 224, 219, 163));
+        newGameButton.setFont(new Font("Bold Serif", Font.BOLD, 16));
+        newGameButton.setBorder(border);
+        newGameButton.setPreferredSize(new Dimension(330, 60));
+        newGameButton.setFocusPainted(false);
+        newGameButton.addMouseListener(this);
+        win.setPreferredSize(new Dimension(330, 0));
+        add(panel);
+        panel.setBackground(new Color(1));
+        spelGrid.setPreferredSize(new Dimension(400, 300));
+        pack();
+        setVisible(true);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+    public void newGrid(JButton[][] buttons, JPanel spelGrid, int r, int g, int b) {
+        spelGrid.removeAll();
+        spelGrid.setLayout(new GridLayout(4, 4));
+        int[] räknare = new int[16];
+        int i=0;
+
+
+        for (int row = 0; row < buttons.length; row++) {//mindre än 4...
+            for (int column = 0; column < buttons[row].length; column++) {//mindre än 4...
+
+                try {
+                    räknare[i] = Integer.parseInt(buttons[row][column].getText());
+                    buttons[row][column] = new JButton(String.valueOf(räknare[i]));
+
+                    spelGrid.add(buttons[row][column]);
+                    buttons[row][column].addActionListener(this);
+                }catch (NumberFormatException e){
+                    buttons[row][column] = new JButton(" ");
+                    spelGrid.add(buttons[row][column]);
+                    buttons[row][column].addActionListener(this);
+                }
+                buttons[row][column].setBackground(new Color(r, g, b));
+                buttons[row][column].setForeground(new Color(7, 26, 14, 163));
+                buttons[row][column].setFont(new Font("Bold Serif", Font.BOLD, 16));
+                buttons[row][column].setBorder(border);
+                buttons[row][column].setFocusPainted(false);
+                buttons[row][column].addMouseListener(this);
+                r+=3;
+                g+=4;
+                b+=3;
+                i++;
+            }
+        }
+    }
+
+    public void newGameGrid(JButton[][] buttons, JPanel spelGrid, int r, int g, int b) {
+        spelGrid.removeAll();
+        spelGrid.setLayout(new GridLayout(rows, columns));
+        Integer[] räknare = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 0};
+        List<Integer> räknareList = Arrays.asList(räknare);
+        Collections.shuffle(räknareList);
+        räknareList.toArray(räknare);
+        int i=0;
+        for (int row = 0; row < buttons.length; row++) {
+            for (int column = 0; column < buttons[row].length; column++) {
+
+                if(räknare[i]==0){
+                    buttons[row][column] = new JButton(" ");
+                    spelGrid.add(buttons[row][column]);
+                    buttons[row][column].addActionListener(this);
+                } else {
+                    try {
+                        buttons[row][column] = new JButton(String.valueOf(räknare[i]));
+                        spelGrid.add(buttons[row][column]);
+                        buttons[row][column].addActionListener(this);
+                    } catch (NumberFormatException e) {
+                        System.out.println("HOW?");
+                    }
+                }
+
+                buttons[row][column].setBackground(new Color(r, g, b));
+                buttons[row][column].setForeground(new Color(7, 26, 14, 163));
+                buttons[row][column].setFont(new Font("Bold Serif", Font.BOLD, 16));
+                buttons[row][column].setBorder(border);
+                buttons[row][column].setFocusPainted(false);
+                buttons[row][column].addMouseListener(this);
+                i++;
+            }
+        }
+    }
+
+    public List<String> JButtonsToString(JButton[][] buttons){
+        ArrayList<String> buttonsToString = new ArrayList<>();
+        for (int row = 0; row < buttons.length; row++) {
+            for (int column = 0; column < buttons[row].length; column++) {
+                buttonsToString.add(buttons[row][column].getText());
+            }
+        }
+        return buttonsToString;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButtonArrayClass jbac = new JButtonArrayClass(rows, columns);
+
+        if(e.getSource() == newGameButton) {
+            newGameGrid(buttons, spelGrid, r, g, b);
+            win.setPreferredSize(new Dimension(0,0));
+            } else {
+            ((JButton) e.getSource()).setBackground(Color.red);
+            System.out.println("Chosen number\n" + ((JButton) e.getSource()).getText());
+            buttons = jbac.returnJButtonArray(((JButton) e.getSource()).getText(), buttons);
+            newGrid(buttons, spelGrid, r, g, b);
+
+            if(JButtonsToString(buttons).equals(stringList)){
+                win.setText("WINNER!!");
+                win.setFont(new Font("Monospaced", Font.BOLD, 18));
+                panel.setBackground((new Color(0xFFA359)));
+                win.setForeground(new Color(0xFFFFFF));
+                win.setHorizontalAlignment(CENTER);
+                win.setPreferredSize(new Dimension(330, 50));
+            }
+        }
+            revalidate();
+            repaint();
+        }
+
+
+
+        public static void main (String[]args){
+            new PlayGUI();
+        }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if(e.getSource()==newGameButton)
+            newGameButton.setForeground(new Color(0xFFFFFF));
+            newGameButton.setBackground((new Color(0x1E071E)));
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        if(e.getSource()==newGameButton) {
+            newGameButton.setForeground(new Color(0x6EB298));
+        }
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        if(e.getSource()==newGameButton)
+            newGameButton.setBackground((new Color(0x130F1C)));
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        if(e.getSource()==newGameButton) {
+            newGameButton.setBackground((new Color(0x1B3D30)));
+        }
+    }
+}
+
+
+
+
